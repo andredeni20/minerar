643b353331f0379285061b57fa12787375cd6939
andredeni20 <83710449+andredeni20@users.noreply.github.com>
2021-05-05 10:34:30 -0300

    Add files via upload

---
+++ minerar.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+apt-get update -y; apt-get upgrade -y
+apt-get install htop
+mkdir cpuminer
+cd cpuminer
+git clone https://github.com/luisvasquez/cpuminer-easy
+cd cpuminer-easy
+./build-ubuntu.sh
+./cpuminer -a yescrypt -o stratum+tcp://yescrypt.mine.zpool.ca:6233 -u D6g75t4KT2es1UULfTn5CF3SQtZG49PmJt -p c=DOGE
+
