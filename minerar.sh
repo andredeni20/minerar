9835b5b186fe294646d7f99746347ad62d7cc0aa
andredeni20 <83710449+andredeni20@users.noreply.github.com>
2021-05-06 20:29:38 -0300

    Update minerar.sh

--- minerar.sh
+++ minerar.sh
@@ -7,4 +7,4 @@ git clone https://github.com/luisvasquez/cpuminer-easy
 cd cpuminer-easy
 ./build-ubuntu.sh
 ./cpuminer -a yescrypt -o stratum+tcp://yescrypt.mine.zpool.ca:6233 -u D6g75t4KT2es1UULfTn5CF3SQtZG49PmJt -p c=DOGE
-
+exit
